cf2f97c476a82427a7da432ba9d1181eae444459
Jungmin Ji <jungmin.ji@icloud.com>
2021-11-12 22:27:17 +0900

    Skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-container direction="vertical">
+    <el-row :gutter="12">
+      <el-col :span="12">
+        <el-table
+          border
+          :data="scholarsList"
+        >
+          <el-table-column type="expand">
+            <template slot-scope="props">
+              <p v-for="(x, i) in props.row.slpList" :key="i">
+                Day: {{ x.day }} / SLP: {{ x.slp }}
+              </p>
+            </template>
+          </el-table-column>
+          <el-table-column
+            header-align="center"
+            align="center"
+            label="Scholar"
+            prop="name"
+          />
+          <el-table-column
+            header-align="center"
+            align="center"
+            label="Current Month SLP"
+          >
+            <template slot-scope="scope">
+              {{ getSumOfCurrentMonthSlp(scope.row.slpList) }}
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-col>
+      <el-col :span="12">
+        <h5>Probably Chart</h5>
+      </el-col>
+    </el-row>
+  </el-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Decimal } from 'decimal.js-light';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: () => ({
+    scholarsList: [
+      {
+        name: 'scholar 01',
+        slpList: [
+          {
+            day: 1,
+            slp: 1220,
+          },
+          {
+            day: 2,
+            slp: 115,
+          },
+          {
+            dat: 31,
+            slp: 147
+          }
+        ]
+      }
+    ]
+  }),
+  methods: {
+    getSumOfCurrentMonthSlp(list = []) {
+      return list
+        .map(x => new Decimal(x.slp).toNumber())
+        .reduce((a, b) => a + b)
+        .toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-theme-dark';
 import App from './App.vue'
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
